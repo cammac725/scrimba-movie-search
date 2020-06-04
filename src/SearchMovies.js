270eb5374ccfe8217b0f94d522aca693fb2e0802
cammac <camm@rocktechcomputer.com>
2020-06-04 12:29:02 -0700

    added Card component and finished styling

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,34 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import MovieCard from './MovieCard';
 
 export default function SearchMovies() {
 
+  // states - input query, movies
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (e) => {
     e.preventDefault();
-    console.log("submitting")
 
-    const query = "Jurassic Park";
-
-    const url = `https://api.themoviedb.org/3/movie/550?api_key=db2e9c8cc82dda710557d25b38a00323&query=${query}&page=1&adult=false`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=db2e9c8cc82dda710557d25b38a00323&query=${query}`;
 
     try {
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data);
+      console.log(data.results);
+      setMovies(data.results)
     } catch(err){
       console.log(err)
     }
   }
 
   return (
-   
-    <form className="form" onSubmit={searchMovies}>
-      <label htmlFor="query" className="label">Movie Name</label>
-      <input
-        className="input"
-        type="text"
-        name="query"
-        placeholder="i.e. Jurassic Park" />
-      <button className="button" type="submit">Search</button>
-    </form>
+    <div>
+      <form className="form" onSubmit={searchMovies}>
+        <label htmlFor="query" className="label">Movie Name</label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="i.e. Jurassic Park"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          />
+        <button className="button" type="submit">Search</button>
+      </form>
+      <div className="card-list">
+        {movies.filter(movie => movie.poster_path).map(movie => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
   )
 }
